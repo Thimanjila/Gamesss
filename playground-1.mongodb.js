7acfab48203c376c1286ffd2b52df2c2dc77b244
Thimanjila Udangawe <140135436+Thimanjila@users.noreply.github.com>
2024-09-05 23:26:25 +0530

    Add files via upload

---
+++ playground-1.mongodb.js
@@ -0,0 +1,32 @@
+/* global use, db */
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use('queens');
+
+
+        db.players.insertMany([
+            {
+                name: 'Sachini',
+                solutions: []
+            },
+            {
+                name: 'Dilsha',
+                solutions: []
+            },
+            {
+                name: 'Shyami',
+                solutions: []
+            },
+            {
+                name: 'suki',
+                solutions: []
+            },
+            {
+                name: 'Rocky',
+                solutions: []
+            }
+        ]);
+        
+
